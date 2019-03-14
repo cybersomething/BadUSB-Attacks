bf32d00785b8c406a88e6a2cba9ef6acd7b44cf9
cybersomething <46792838+cybersomething@users.noreply.github.com>
2019-03-14 16:58:26 +0000

    Add files via upload

---
+++ rickroll.ino
@@ -0,0 +1,19 @@
+//This DigiSpark script opens up Rick Astley's - Never Gonna Give You Up and also a fake Windows update screen and then maximizes it using F11
+#include "DigiKeyboard.h"
+void setup() {
+  //empty
+}
+void loop() {
+  DigiKeyboard.delay(2000);
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.print("https://youtu.be/dQw4w9WgXcQ?t=43s");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.print("http://fakeupdate.net/wnc/");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_F11);
+  for(;;){ /*empty*/ }
+}
