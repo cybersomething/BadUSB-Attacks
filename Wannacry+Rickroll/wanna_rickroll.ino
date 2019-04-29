8eb0a53c8159ae873c8c7ccbb09a360529df1015
cybersomething <46792838+cybersomething@users.noreply.github.com>
2019-04-29 14:44:40 +0100

    Update wanna_rickroll.ino

--- Wannacry+Rickroll/wanna_rickroll.ino
+++ Wannacry+Rickroll/wanna_rickroll.ino
@@ -1,18 +1,18 @@
-#include "DigiKeyboard.h"
+include "DigiKeyboard.h"
 void setup() {
-  //empty
+  // put your setup code here, to run once:
 }
 void loop() {
   DigiKeyboard.delay(2000);
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.print("https://youtu.be/dQw4w9WgXcQ?t=43s");
+  DigiKeyboard.print("https://www.youtube.com/watch?v=PiCMKQ2TmfQ");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.print("http://fakeupdate.net/wnc/");
+  DigiKeyboard.print("https://urlzs.com/uvtn");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(1000);
   DigiKeyboard.sendKeyStroke(KEY_F11);
   for(;;){ /*empty*/ }
 }
